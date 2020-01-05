2eb8953ad4be618a8343d590ecdd27fbf7004a17
naoko <naoko1010hh>
2020-01-05 20:52:14 +0900

    リダイレクト部分の修正

---
+++ contact/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@serene_jp">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" contet="linux.bex.jp">
+    <meta property="og:locale" content="ja_JP">
+    <meta property="og:image" content="http://linux.bex.jp/img/icon/150.png">
+    <link rel="apple-touch-icon-precomposed" href="http://linux.bex.jp/img/icon/150.png">
+    <meta name="msapplication-TileImage" content="http://linux.bex.jp/img/icon/150.png">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <link rel="shortcut icon" href="http://linux.bex.jp/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="http://linux.bex.jp/img/icon/16.png" sizes="16x16" type="image/png">
+    <link rel="icon" href="http://linux.bex.jp/img/icon/32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="http://linux.bex.jp/img/icon/48.png" sizes="48x48" type="image/png">
+    <link rel="icon" href="http://linux.bex.jp/img/icon/64.png" sizes="64x64" type="image/png">
+    <title>問い合わせ - SereneLinux Official Site</title>
+    <meta property="og:title" content="問い合わせ - SereneLinux Official Site">
+    <meta name="description" content="SereneLinuxに関する、問い合わせはこちら。バグ不具合の報告はユーザーフォーラムまたはDiscordコミュニティーへ">
+    <meta property="og:description" content="SereneLinuxに関する、問い合わせはこちら。バグ不具合の報告はユーザーフォーラムまたはDiscordコミュニティーへ">
+    <meta property="og:url" content="http://linux.bex.jp/contact/">
+    <link rel="canonical" href="http://linux.bex.jp/contact/">
+    <link rel="alternate" href="http://linux.bex.jp/contact/" hreflang="ja-jp">
+    <link rel="alternate" href="http://linux.bex.jp/en/contact/" hreflang="en-us">
+    <link rel="alternate" href="http://linux.bex.jp/contact/" hreflang="x-default">
+    <meta name="google" content="notranslate">
+    <link rel="stylesheet" href="/css/default.css">
+    <link rel="stylesheet" href="/css/contact.css">
+</head>
+<body>
+  <input type="checkbox" name="gnav-bin" id="gnav-bin">
+  <header id="head">
+    <h1><a href="/"><img src="/img/icon/64.png" alt="セレネリナックス"><span>SereneTeam</span></a></h1>
+    <label class="gnav-open" for="gnav-bin"><i class="material-icons">menu_open</i></label>
+    <nav id="gnav">
+      <label class="gnav-close" for="gnav-bin"><i class="material-icons">close</i></label>
+      <ul id="gnav-ul">
+        <li><a href="/">ホーム</a></li>
+        <li><a href="/forum/">フォーラム</a></li>
+        <li><a href="/download/">ダウンロード</a></li>
+        <li><a href="/blog/">ブログ</a></li>
+        <li><a href="/links/">リンク集</a></li>
+        <li><a href="/donation/">寄付する</a></li>
+        <li><a href="/team/">チーム</a></li>
+      </ul>
+    </nav>
+  </header>
+  <article class="link">
+    <h1>SereneLinux 問い合わせ</h1>
+    <form action=""></form>
+  </article>
+  <a href="/en/links/" id="language">
+    <img src="data:image/gif;base64,R0lGODdhIAARAPUAAAQujIRCRNze7BQ6lBQujOQCFAw2lPzm7AwujERerCxKnBw+lOTi7FxytIxubHSGvDxapKy21OwuPExmrGR6tAwyjGyCvJSizCRGnIyOjOQSJDRSpOzq9PRqdPz6/KSu1PS2vIRKTPze3BQ2lOQKHAQylIwqNAwylAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACwAAAAAIAARAAAGr8DFhDAsKhTDRoDEbDqfTWFjMm1AJhMIZXMBeb/g8HexLSus5/NHxG6732wQQWGhk5EN8iAk6fv/gH0aeg8jFAZWZAAPER6Oj5CRjgwVJ4cWABQYWosADh2goaKjoQOXmSdYihYZYq5hCJ2HmpyHEQe4ubq7uCIEp4eqsyYFxcbHyMYCHMzNzs280boir9XWYaTZ2tugDJLf4OEMGoHl5ucaIHDr7O3X79ZQ8vP0TEEAOw==" alt="アメリカ国旗">
+    <span>English</span>
+  </a>
+  <footer id="foot"><small>&copy; 2019-2020 SereneLinux</small></footer>
+</body>
+</html>
